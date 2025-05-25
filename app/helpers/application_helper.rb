36a8c961f2e0f539a27e064561fc09da0690ca00
Mason <bigmasonwang@gmail.com>
2025-05-25 22:12:49 +0930

    Add logo helper method and integrate it into Devise layout and navbar for consistent branding.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def logo(class_name: "navbar-brand")
+    link_to(root_path, class: "#{class_name}") do
+      content_tag(:span, class: "bi bi-lock-fill") do
+        "PassManager"
+      end
+    end
+  end
 end
