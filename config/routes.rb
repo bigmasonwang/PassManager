cfd47149aee95b28f2db3c7ec93bcee5e45d7fa9
Mason <bigmasonwang@gmail.com>
2025-05-25 21:13:59 +0930

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "about", to: "about#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
+  get "about", to: "about#index"
   get "home", to: "home#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
